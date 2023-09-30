98ac0d49651a4e6dc9b0d0afd6a445c65ba83429
robin-dutt <80968579+robin-dutt@users.noreply.github.com>
2023-09-30 05:37:29 +0530

    app.py

--- app.py
+++ app.py
@@ -2,23 +2,6 @@ import streamlit as st
 import dashboard
 import classifyPage
 
-# Add custom CSS to style the sidebar
-st.markdown(
-    """
-    <style>
-    .sidebar .sidebar-content {
-        background-color: #f8f9fa; /* Background color */
-        padding: 20px; /* Padding inside the sidebar */
-        border-radius: 10px; /* Rounded corners */
-    }
-    .sidebar .sidebar-content .sidebar-radio {
-        margin-top: 10px; /* Margin between radio buttons */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
 # Set Streamlit page configuration
 st.set_page_config(
     page_title="Deforgify",
@@ -32,7 +15,7 @@ PAGES = {
     "Classify Image": {"page": classifyPage, "icon": "🖼️"}
 }
 
-# Sidebar styling
+# Sidebar styling using custom CSS
 st.sidebar.title("SAFIRE")
 st.sidebar.markdown("SAFIRE is a tool that utilizes the power of Deep Learning to distinguish Real images from the Fake ones.")
 st.sidebar.subheader('Navigation')
@@ -42,6 +25,21 @@ selection = st.sidebar.radio("", list(PAGES.keys()), format_func=lambda page_nam
 
 # Get the selected page and its corresponding icon
 selected_page = PAGES[selection]['page']
+
+# Apply custom CSS styles
+custom_css = """
+<style>
+.sidebar .sidebar-content {
+    background-color: #f8f9fa; /* Background color */
+    padding: 20px; /* Padding inside the sidebar */
+    border-radius: 10px; /* Rounded corners */
+}
+.sidebar .sidebar-content .sidebar-radio {
+    margin-top: 10px; /* Margin between radio buttons */
+}
+</style>
+"""
+st.markdown(custom_css, unsafe_allow_html=True)
 
 # Display the selected page
 selected_page.app()
